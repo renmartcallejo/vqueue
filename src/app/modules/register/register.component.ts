a2fcc7c1dfff1fb7dde1081ba689422d715ec28c
Renmart Callejo <aladeengem@gmail.com>
2019-05-13 14:42:24 +0800

    refactor code in register page

--- src/app/modules/register/register.component.ts
+++ src/app/modules/register/register.component.ts
@@ -33,47 +33,73 @@ export class RegisterComponent implements OnInit {
     }
   }
 
-  async register(){
-    this.service.changeLoadingState(true);
+  alertState(state, message){
 
-    if(!this.validatePassword()){
-
-      let response = await this.service.register(this.email, this.password)
-          .then(response => { return response })
-          .catch(err => { return err });
-    
-      if(response.code){
-        this.showAlert = true;
-        this.alert.type = 'alert-danger';
-        this.alert.message = response.message;
+    let alert = {
+      responseError: {
+        type: 'alert-danger',
+        message: message
+      },
+      successRegister: {
+        type: 'alert-success',
+        message: message
+      },
+      distinctPassword : {
+        type: 'alert-danger',
+        message: message
       }
-      else if(response.operationType == "signIn"){
-
-        let data = {
-          'admin_id': response.user.uid,
-          'company': {
-            'comp_id': '',
-            'name': '',
-          },
-          'email': response.user.email,
-          'name': response.user.displayName
-        }
-
-        await this.service.addUserAdmin(data).then(response => console.log(response));
-
-        this.showAlert = true;
-        this.alert.type = 'alert-success';
-        this.alert.message = "You have registered successfully! ";
-      }
-      
-     this.service.changeLoadingState(false);
     }
-    else{
-      this.showAlert = true;
-      this.alert.message = "Password and repeat password should be the same";
-      this.service.changeLoadingState(false);
+
+    this.showAlert = true;
+
+    if(state == 1){
+      this.alert.type = alert.responseError.type;
+      this.alert.message = alert.responseError.message
     }
+    else if(state == 2){
+      this.alert.type = alert.successRegister.type;
+      this.alert.message = alert.successRegister.message;
+    }
+    else if(state == 3){
+      this.alert.type = alert.distinctPassword.type;
+      this.alert.message = alert.distinctPassword.message;
+    }   
 
   }
 
+  async storeAdminToDB(response){
+  
+    let data = {
+      'admin_id': response.user.uid,
+      'company': {
+        'comp_id': '',
+        'name': '',
+      },
+      'email': response.user.email,
+      'name': response.user.displayName
+    }
+    return await this.service.addUserAdmin(data).then(response => console.log(response));
+  }
+
+  async register(){
+    this.service.changeLoadingState(true);
+    if(!this.validatePassword()){
+      let response = await this.service.register(this.email, this.password)
+          .then(response => { return response })
+          .catch(err => { return err });
+
+      if(response.code){
+        this.alertState(1, response);
+      }
+      else if(response.operationType == "signIn"){
+        await this.storeAdminToDB(response);
+        this.alertState(2, "You have registered successfully!");
+      }    
+     this.service.changeLoadingState(false);
+    }
+    else{
+      this.alertState(3, "Password and repeat password should be the same");
+      this.service.changeLoadingState(false);
+    }
+  }
 }
