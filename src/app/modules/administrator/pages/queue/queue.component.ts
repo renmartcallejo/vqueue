6769753cddcef7d5c96f65a0401f034e94c22838
Renmart Callejo <aladeengem@gmail.com>
2019-05-23 18:00:30 +0800

    add queue layout

--- src/app/modules/administrator/pages/queue/queue.component.ts
+++ src/app/modules/administrator/pages/queue/queue.component.ts
@@ -3,10 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-queue',
   templateUrl: './queue.component.html',
-  styleUrls: ['./queue.component.sass']
+  styleUrls: ['./queue.component.scss']
 })
 export class QueueComponent implements OnInit {
 
+
+  users: Array<any> = [
+    {
+      id: '1',
+      name: 'Gem',
+      creation: 'asda',
+      color:'asd'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
